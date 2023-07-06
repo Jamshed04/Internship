907cbc94cfd187223a53d5df248facf72e040aae
Jamshed <137492317+Jamshed04@users.noreply.github.com>
2023-07-06 21:38:36 +0700

    Delete task_7.py

--- task_7.py
+++
@@ -1,8 +0,0 @@
-string = input("Введите строку: ")
-for i in range(len(string)):
-    if string[i] == string[i].title():
-        print(string[i].lower(), end='')
-    elif string[i] == string[i].lower():
-        print(string[i].title(), end='')
-    else:
-        print(" ", end='')
