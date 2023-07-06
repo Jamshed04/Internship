ef0b547edaca37ec3d6c574e4430b98aca07a6eb
Jamshed <137492317+Jamshed04@users.noreply.github.com>
2023-07-06 21:38:56 +0700

    Delete task_5.py

--- task_5.py
+++
@@ -1,9 +0,0 @@
-score = int(input("Введите количество очков: "))
-if score == 3:
-    print("Выигрыш")
-elif score == 1:
-    print("Ничья")
-elif score == 0:
-    print("Проигрыш")
-else:
-    print("Error")
